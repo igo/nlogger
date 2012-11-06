7ae63656f69d47c2fa73a9bf464620d99c98ec5b
Joost de Vries <joost@closealert.com>
2012-11-06 20:35:19 +0100

    Fixed error and added example

--- examples/my-modules/runtime-config.js
+++ examples/my-modules/runtime-config.js
@@ -2,3 +2,4 @@ var logger = require('../../lib/nlogger').logger('my-custom-name',{level:{'*':'w
 
 logger.info('Because of the runtime cfg, this will be ignored.');
 logger.warn('But this is visible. Notice the custom name too :-)');
+logger.error('And here is a formated string with a number %d and an object %s',5,require('util').inspect({a:1}));
